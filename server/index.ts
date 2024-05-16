98106b84f3a7923676cf8dc00e7e12ffee2bb720
phuc-create <phuc82908@gmail.com>
2024-05-16 10:46:21 +0700

    git commit -m "server update"

--- server/index.ts
+++ server/index.ts
@@ -1,47 +1,61 @@
-let messages: { username: string, message: string }[] = []
+import type { ServerWebSocket } from "bun"
+
+
+type TMessage = {
+  username: string, message: string
+}
+
+type TUser = {
+  id: string,
+  username: string,
+}
+let messages: TMessage[] = []
+let users: TUser[] = []
+
 const server = Bun.serve<{ username: string }>({
   port: 4000,
   fetch(req, server) {
-    const url = new URL(req.url)
     // if (url.pathname === "/chat") {
     console.log(`upgrade!`)
-    const username = "user_" + Math.random().toString().slice(12)
-    const success = server.upgrade(req, { data: { username } })
+    // const username = "user_" + Math.random().toString().slice(12)
+    const success = server.upgrade(req
+      // , { data: { username } }
+    )
     return success
       ? undefined
       : new Response("WebSocket upgrade error", { status: 400 })
-    // }
-
-    // return new Response("Hello world")
   },
   websocket: {
     open: (ws) => {
-      const msg = `${ws.data.username} has entered the chat`
-      ws.subscribe("the-group-chat")
-      messages.push({ username: ws.data.username.toString(), message: msg })
-      ws.publish("the-group-chat", JSON.stringify(messages))
-      // ws.(JSON.stringify([...messages, data]))
+      ws.subscribe("group-chat")
+      // socketServer.push(ws)
+      // const msg = `${ws.data.username} has entered the chat`
+      const msg = messages.length ? "Welcome to the chat" : "The new chat was opened!"
+      messages.push({ username: "Bot", message: msg })
+      // sendToEveryone(messages)
+      // ws.publish("group-chat", JSON.stringify(messages))
+      server.publish("group-chat", JSON.stringify(messages))
     },
     message: (ws, message) => {
-      console.log("this is incomming message: ", message)
+      const msg = JSON.parse(message.toString()) as TMessage
+      console.log("this is incomming message: ", msg)
       // this is a group chat
       // so the server re-broadcasts incoming message to everyone
-      const data = { username: ws.data.username.toString(), message: message.toString() }
+      const data = { username: msg.username, message: msg.message }
       messages.push(data)
-      // ws.publish("the-group-chat", JSON.stringify(messages))
-      ws.send(JSON.stringify([...messages, data]))
+
+      // sendToEveryone(messages)
+      server.publish("group-chat", JSON.stringify(messages))
     },
-    close: (ws) => {
-      const msg = `${ws.data.username} has left the chat`
-      ws.unsubscribe("the-group-chat")
-      ws.publish("the-group-chat", msg)
+    close: (ws, code, reason) => {
+      ws.unsubscribe("group-chat")
+      const data = { username: "Bot", message: reason }
+      messages.push(data)
+      server.publish("group-chat", JSON.stringify(messages))
+      // socketServer.filter(open => open.data.username !== ws.data.username
+      // )
     },
   },
 })
 
 console.log(`Listening on ${server.hostname}:${server.port}`)
-
-function getUsernameFromReq(req: Request) {
-  console.log(req)
-  throw req.body
-}
