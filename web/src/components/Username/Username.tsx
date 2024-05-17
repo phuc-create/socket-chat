8dc2a81b67d236b7b47f034e6f050386771cae2d
phuc-create <phuc82908@gmail.com>
2024-05-17 16:27:48 +0700

    haha

--- web/src/components/Username/Username.tsx
+++ web/src/components/Username/Username.tsx
@@ -6,6 +6,22 @@ interface IUsername {
   handleOpenChat: () => void
 }
 const Username: React.FC<IUsername> = ({ value, handleOpenChat }) => {
+
+  /**
+   * input: array of numbers
+   * output: how many odd number in array
+   */
+
+  const howManyOdd = (arr: number[]) => arr.reduce((s, n) => s += n & 1, 0)
+
+  console.log(howManyOdd([])) // 0
+  console.log(howManyOdd([2, 4, 6, 8])) // 0
+  console.log(howManyOdd([1, 3, 5, 7])) // 4
+  console.log(howManyOdd([1, 2, 3, 4, 5])) // 3
+  console.log(howManyOdd([2, 7])) // 1
+  console.log(howManyOdd([1, 1, 1, 2])) // 3
+  console.log(howManyOdd([-1001, 2002, 3003, -4004])) // 2
+  console.log(howManyOdd([1.5, 2, 3.14, 4])) // 2
   const { updateUser, userInfor } = useMessage()
 
   const handleChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
